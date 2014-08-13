01f369204901858e8f18caa3e9909280973ee44f
vexing <andreas.hagglund@vizzit.se>
2014-08-13 05:58:53 -0700

    Revert "Revert "VizzitLogFetcher first commit""
    This reverts commit fe532de3c386aa490078e3716681f94df58b2578.

---
+++ LogHandler/RunVizzitJob.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LogHandler
+{
+    public class RunVizzitJob
+    {
+        string fileName;
+        string zipFile;
+
+        public RunVizzitJob(string logPath, string customerId, List<string> dateArray)
+        {
+            foreach(string date in dateArray)
+            {
+                fileName = SetFileName(date);
+                string destFileName = fileName.Remove(0, 2);
+
+                GetLog.CopyLog(logPath, fileName, destFileName);
+                zipFile = ZipLog.ZipLogfile(destFileName);
+                SendLog.SendToVizzit(zipFile, customerId);
+                CleanUp(destFileName);
+            }
+        }
+
+        private void CleanUp(string destFileName)
+        {
+            File.Delete(zipFile);
+            File.Delete(destFileName);
+        }
+        
+        private string SetFileName(string date)
+        {
+            return "u_ex" + date +".log";
+        }
+    }
+}
