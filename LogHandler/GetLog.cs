4404c5e3d11eaf5039d1e6ead88379b7db7c748a
vexing <andreas.hagglund@vizzit.se>
2014-08-13 05:58:28 -0700

    VizzitLogFetcher first commit
    Fetch IIS logfiles

---
+++ LogHandler/GetLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LogHandler
+{
+    public static class GetLog
+    {
+        public static void CopyLog(string logPath, string fileName, string destFileName)        
+        {           
+            File.Copy(Path.Combine(logPath, fileName), Path.Combine(System.IO.Directory.GetCurrentDirectory(), destFileName));
+        }
+    }
+}
